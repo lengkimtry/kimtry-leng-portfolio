e4bf4f42e57f43d11f2ebf1f6bfcd33acebe774f
lengkimtry <kimt47494@gmail.com>
2024-08-08 22:39:06 +0700

    fixed the Imge

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -2,11 +2,10 @@ import Image from "next/image";
 import React from "react";
 
 function Body() {
-
   return (
     <div className="bg-[#4c4c4c] opacity-100 lg:pt-[6rem] h-auto pt-[1rem] sm:h-[44rem]">
       <div
-        className="relative max-w-7xl mx-auto px-4 sm:px-6 flex flex-col sm:flex-row justify-between items-center mt-[2rem]"
+        className="relative max-w-7xl mx-auto px-4 sm:px-6 flex flex-col sm:flex-row justify-between items-center mt-[5rem]"
         style={{ height: "auto" }}
       >
         <div className="relative w-full sm:w-1/2 mb-8 sm:mb-0">
@@ -14,15 +13,15 @@ function Body() {
             className="object-cover rounded-t ml-0 sm:mr-0"
             src="/my_picture2.jpg"
             alt="My Picture 2"
-            width={500}
-            height={400}
-            style={{ maxWidth: "100%", height: "auto" }} 
+            width={377}
+            height={350}
+            style={{ width: "auto", height: "auto" }}
             priority
-            loading="eager"// Ensures responsiveness
+            loading="eager" // Disable lazy loading
           />
         </div>
         <div className="absolute sm:ml-6  top-0 left-0 w-full h-full bg-gradient-to-r from-[#000000] to-[#000000] opacity-30 z-10"></div>
-        <div className="relative w-full sm:w-1/2 px-6 pl-0 py-12 text-center flex flex-col justify-center z-20">
+        <div className="relative w-full sm:w-1/2 px-6 pl-0 py-12 text-center flex flex-col pr-[13rem] justify-center z-20">
           <h1 className="text-3xl sm:text-4xl font-bold leading-tight text-white">
             Welcome to Kimtry Lengs Portfolio!
           </h1>
