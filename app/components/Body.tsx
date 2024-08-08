a786c429ffd0fe2329ceb8556d48a23a9458c081
lengkimtry <kimt47494@gmail.com>
2024-08-08 23:06:29 +0700

    fixed Image

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -5,23 +5,24 @@ function Body() {
   return (
     <div className="bg-[#4c4c4c] opacity-100 lg:pt-[6rem] h-auto pt-[1rem] sm:h-[44rem]">
       <div
-        className="relative max-w-7xl mx-auto px-4 sm:px-6 flex flex-col sm:flex-row justify-between items-center mt-[5rem]"
+        className="relative max-w-7xl mx-auto px-4 sm:px-6 flex flex-col sm:flex-row justify-between items-center mt-[1rem]"
         style={{ height: "auto" }}
       >
         <div className="relative w-full sm:w-1/2 mb-8 sm:mb-0">
           <Image
-            className="object-cover rounded-t ml-0 sm:mr-0"
-            src="/my_picture2.jpg"
+            src={"/my_picture2.jpg"}
             alt="My Picture 2"
             width={377}
             height={350}
-            style={{ width: "auto", height: "auto" }}
+            layout="responsive" // Makes the image responsive
+            objectFit="cover" // Ensures the image covers the container while maintaining aspect ratio
+            className="rounded-t"
             priority
             loading="eager" // Disable lazy loading
           />
         </div>
-        <div className="absolute sm:ml-6  top-0 left-0 w-full h-full bg-gradient-to-r from-[#000000] to-[#000000] opacity-30 z-10"></div>
-        <div className="relative w-full sm:w-1/2 px-6 pl-0 py-12 text-center flex flex-col pr-[13rem] justify-center z-20">
+        <div className="absolute sm:ml-6 top-0 left-0 w-full h-full bg-gradient-to-r from-[#000000] to-[#000000] opacity-30 z-10"></div>
+        <div className="relative w-full sm:w-1/2 px-6 pl-0 py-12 text-center flex flex-col lg:m-[5rem] justify-center z-20">
           <h1 className="text-3xl sm:text-4xl font-bold leading-tight text-white">
             Welcome to Kimtry Lengs Portfolio!
           </h1>
